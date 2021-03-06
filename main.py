a3187c844d84a89957a78b120284cc892cf273f7
Atalaxian <39862662+Atalaxian@users.noreply.github.com>
2021-03-06 23:02:28 +0300

    Создана базовая модель

---
+++ main.py
@@ -0,0 +1,73 @@
+import sys
+import os
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+import pandas as pd
+import numpy as np
+from networks import Network
+import tensorflow as tf
+from tensorflow.python.keras import backend as K
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtCore
+from main_window import Ui_Form
+from keras.models import load_model
+import joblib
+
+
+def fit():
+    config = tf.compat.v1.ConfigProto(device_count={'GPU': 1, 'CPU': 6})
+    sess = tf.compat.v1.Session(config=config)
+    K.set_session(sess)
+    my_dataset = pd.read_excel('input.xlsx', sheet_name=0, na_values=np.nan)
+    mynetwork = Network(my_dataset)
+    mynetwork.first_network(copy=True)
+    mynetwork.second_network(copy=True)
+
+
+class Window(QWidget, Ui_Form):
+    std_model_1 = None
+    std_model_2 = None
+    robust_model = None
+    network_1 = None
+    network_2 = None
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.load_models()
+        self.pushButton.clicked.connect(self.predict)
+
+    def load_models(self):
+        self.std_model_1 = joblib.load('Models/std_scaler_first.pkl')
+        self.std_model_2 = joblib.load('Models/std_scaler_sec.pkl')
+        self.robust_model = joblib.load('Models/rob_scaler_sec.pkl')
+        self.network_1 = load_model('Models/model_first.h5')
+        self.network_2 = load_model('Models/model_sec.h5')
+
+    @QtCore.pyqtSlot()
+    def predict(self):
+        try:
+            p = float(self.lineEdit.text())
+            t = float(self.lineEdit_2.text())
+        except ValueError:
+            return
+        if p < 5:
+            features = self.std_model_2.transform(np.array([[p, t]]))
+            print(features)
+            raw_result = self.network_2.predict(features)
+            result = self.robust_model.inverse_transform(raw_result)
+            self.result.setText(str(result[0][0]))
+        else:
+            features = self.std_model_1.transform(np.array([[p, t]]))
+            print(features)
+            result = self.network_1.predict(features)
+            self.result.setText(str(result[0][0]))
+
+if __name__ == '__main__':
+    #my_dataset = pd.read_excel('input.xlsx', sheet_name=0, na_values=np.nan)
+    #network = Network(my_dataset)
+    #network.first_network(copy=True)
+    #network.second_network(copy=True)
+    qapp = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(qapp.exec())
---
+++ networks.py
@@ -0,0 +1,153 @@
+import joblib
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from keras import models, layers
+
+
+class Network:
+    dataset = None
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    @staticmethod
+    def graph_loss(epoch_count, training_loss, test_loss):
+        plt.clf()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(epoch_count, training_loss, 'r--')
+        ax.plot(epoch_count, test_loss, 'b-')
+        plt.legend(['Потеря на тренировке', 'Потеря на тестировании'])
+        plt.xlabel('Эпоха')
+        plt.ylabel('Потеря')
+        plt.show()
+
+    @staticmethod
+    def graph_accuracy(epoch_count, training_accuracy, test_acccuracy):
+        plt.clf()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(epoch_count, training_accuracy, 'r--')
+        ax.plot(epoch_count, test_acccuracy, 'b-')
+        plt.legend(['Точность на тренировке', 'Точность на тестировании'])
+        plt.xlabel('Эпоха')
+        plt.ylabel('Точность')
+        plt.show()
+
+    def second_network(self, copy=False):
+        if copy:
+            dataset = self.dataset.copy()
+        else:
+            dataset = self.dataset
+        std_scaler = StandardScaler()
+        robust_scaler = preprocessing.RobustScaler()
+        np.random.seed(0)
+        dataset = dataset.dropna()
+        print('Изначальная размерность:')
+        print(dataset.shape)
+        dataset = dataset[dataset['p'] < 5]
+        print('Конечная размерность:')
+        print(dataset.shape)
+        target = np.array(dataset['v'])
+        dataset = dataset.drop('v', axis=1)
+        target = robust_scaler.fit_transform(target.reshape(-1, 1))
+        print(target)
+        features_std = std_scaler.fit_transform(dataset)
+        joblib.dump(robust_scaler, 'Models/rob_scaler_sec.pkl')
+        joblib.dump(std_scaler, 'Models/std_scaler_sec.pkl')
+        features_train, features_test, target_train, target_test = \
+            train_test_split(features_std, target, random_state=1, train_size=0.8)
+        checkpoint = [ModelCheckpoint(filepath='Models/model_sec.h5', save_best_only=True, monitor='val_loss')]
+        mynetwork = models.Sequential()
+        mynetwork.add(layers.Dense(
+            units=64,
+            activation='relu',
+            input_shape=(features_train.shape[1],)))
+        mynetwork.add(layers.Dense(
+            units=32,
+            activation='relu',
+            input_shape=(features_train.shape[1],)))
+        mynetwork.add(layers.Dense(units=1))
+        mynetwork.compile(loss='mse',
+                          optimizer='RMSprop',
+                          metrics=['mse'])
+        history = mynetwork.fit(
+            features_train,
+            target_train,
+            epochs=5000,
+            callbacks=checkpoint,
+            verbose=0,
+            batch_size=500,
+            validation_data=(features_test, target_test)
+        )
+        target_1 = mynetwork.predict(features_test)
+        print(history)
+        training_loss = history.history['loss']
+        print(training_loss)
+        test_loss = history.history['val_loss']
+        print(test_loss)
+        epoch_count = range(1, len(training_loss) + 1)
+        self.graph_loss(epoch_count, training_loss, test_loss)
+        for x, elem in enumerate(target_1):
+            text = str(elem) + ' ' + str(target_test[x])
+            print(text)
+
+    def first_network(self, copy=False):
+        if copy:
+            dataset = self.dataset.copy()
+        else:
+            dataset = self.dataset
+        std_scaler = StandardScaler()
+        np.random.seed(0)
+        dataset = dataset.dropna()
+        print('Изначальная размерность:')
+        print(dataset.shape)
+        dataset = dataset[dataset['v'] < 200]
+        print('Конечная размерность:')
+        print(dataset.shape)
+        target = np.array(dataset['v'])
+        dataset = dataset.drop('v', axis=1)
+        print(target)
+        features_std = std_scaler.fit_transform(dataset)
+        joblib.dump(std_scaler, 'Models/std_scaler_first.pkl')
+        features_train, features_test, target_train, target_test = \
+            train_test_split(features_std, target, random_state=1, train_size=0.8)
+        checkpoint = [ModelCheckpoint(filepath='Models/model_first.h5', save_best_only=True, monitor='val_loss')]
+        network = models.Sequential()
+        network.add(layers.Dense(
+            units=64,
+            activation='relu',
+            input_shape=(features_train.shape[1],)))
+        network.add(layers.Dense(
+            units=32,
+            activation='relu',
+            input_shape=(features_train.shape[1],)))
+        network.add(layers.Dense(units=1))
+        network.compile(loss='mse',
+                        optimizer='RMSprop',
+                        metrics=['mse'])
+        history = network.fit(
+            features_train,
+            target_train,
+            callbacks=checkpoint,
+            epochs=10000,
+            verbose=0,
+            batch_size=200,
+            validation_data=(features_test, target_test)
+        )
+        target_1 = network.predict(features_test)
+        print(history)
+        training_loss = history.history['loss']
+        print(training_loss)
+        test_loss = history.history['val_loss']
+        print(test_loss)
+        epoch_count = range(1, len(training_loss) + 1)
+        self.graph_loss(epoch_count, training_loss, test_loss)
+        for x, elem in enumerate(target_1):
+            text = str(elem) + ' ' + str(target_test[x])
+            print(text)
+        print(network.predict(std_scaler.transform(np.array([[100, 25]]))))
